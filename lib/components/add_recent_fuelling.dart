c8b6bbfc0c193714f317cdd72cd2a7987e24eb41
Filip Chamier-Ciemiński <45480039+fcieminski@users.noreply.github.com>
2019-12-28 18:23:44 +0100

    form done

--- lib/components/add_recent_fuelling.dart
+++ lib/components/add_recent_fuelling.dart
@@ -1,13 +1,70 @@
 import 'package:flutter/material.dart';
 
 class AddRecentFuelling extends StatefulWidget {
+  // final Function _addRecentFuelling;
+
+  // AddRecentFuelling(this._addRecentFuelling);
+
   @override
   _AddRecentFuellingState createState() => _AddRecentFuellingState();
 }
 
 class _AddRecentFuellingState extends State<AddRecentFuelling> {
+  final distance = TextEditingController();
+  final amount = TextEditingController();
+  final totalCost = TextEditingController();
+
+  void submitForm() {
+    print(distance.text);
+    print(amount.text);
+    print(totalCost.text);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Form(
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              keyboardType: TextInputType.numberWithOptions(),
+              controller: amount,
+              decoration: const InputDecoration(
+                labelText: 'Ilość benzyny',
+              ),
+              onSaved: (String value) {
+                submitForm();
+              },
+            ),
+            TextFormField(
+              keyboardType: TextInputType.numberWithOptions(),
+              controller: totalCost,
+              decoration: const InputDecoration(
+                labelText: 'Koszt benzyny',
+              ),
+              onSaved: (String value) {
+                submitForm();
+              },
+            ),
+            TextFormField(
+              keyboardType: TextInputType.numberWithOptions(),
+              controller: distance,
+              decoration: const InputDecoration(
+                labelText: 'Przejechany dystans',
+              ),
+              onSaved: (String value) {
+                submitForm();
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add,
+        ),
+        onPressed: submitForm,
+      ),
+    );
   }
 }
